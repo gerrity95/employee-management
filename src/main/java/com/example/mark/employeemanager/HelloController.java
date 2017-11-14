8f3f8c36413174b6e64a9003c1f474c1db018beb
Mark Gerrity <gerrity95@gmail.com>
2017-11-14 11:19:42 +0000

    First Commit

---
+++ src/main/java/com/example/mark/employeemanager/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.mark.employeemanager;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
