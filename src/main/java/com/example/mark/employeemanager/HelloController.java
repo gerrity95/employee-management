6d3601ef99d754a4523fad886fb365605ca5ca1e
casha17 <caspha@gmail.com>
2017-11-14 21:50:45 +0100

    Implenteted bootstrap and index.html

--- src/main/java/com/example/mark/employeemanager/HelloController.java
+++ src/main/java/com/example/mark/employeemanager/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.mark.employeemanager;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index.html";
     }
 
 }
